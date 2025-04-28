96dad37f029bf040bed5272e9b8601df3aca2d8f
martinristow <martinristov084@gmail.com>
2025-04-28 21:08:50 +0200

    formatter numbers

---
+++ src/lib/formatters.ts
@@ -0,0 +1,8 @@
+const compactNumberFormatter = new Intl.NumberFormat(undefined, {
+    notation: "compact",
+})
+
+
+export function formatCompactNumber(number: number){
+    return compactNumberFormatter.format(number);
+}
