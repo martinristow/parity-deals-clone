ca9fc4e3bd7d0361e42f2e66a7c8657c20524c5f
martinristow <martinristov084@gmail.com>
2025-04-28 19:42:27 +0200

    marketing layout

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,8 @@
+import {NavBar} from "@/app/(marketing)/_components/NavBar";
+
+export default function MarketingLayout({children}: {children: React.ReactNode}) {
+    return <div className="selection:bg-[hsl(320,65%,52%,20%)]">
+        <NavBar/>
+        {children}
+    </div>
+}
